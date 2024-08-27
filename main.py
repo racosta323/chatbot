d19fb454bae93bb7ca8be1fff1815e446a3ced7b
Rene <racosta323@gmail.com>
2024-08-26 20:30:26 -0500

    creates chat template and handle_conversation method.

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+# give detail here
+template = """
+Answer the question below.
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer:
+"""
 
 model = OllamaLLM(model='llama3')
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
 
-result = model.invoke(input='hello world')
-print(result)
+def handle_conversation():
+    context =  ""
+    print("Welcome to the chatbot. Type exit to quit.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            break
+        result = chain.invoke({'context':'', 'question':user_input})
+        print("Bot: ", result)
+        context += f'\nUser: {user_input}\nAI: {result}'
+
+if __name__ == "__main__":
+    handle_conversation()
